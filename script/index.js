f1abbc765d5a1fd9fd0fffc46000489bd16052dd
kimsuyeon1231 <a01099186626@gmail.com>
2025-11-14 16:46:15 +0900

    헤더 및 배너 슬라이더 작업중

---
+++ script/index.js
@@ -0,0 +1,51 @@
+/* 베스트셀러 */
+const bestSeller = new Swiper('#best_seller .swiper',{
+    slidesPerView : 5,
+    spaceBetween : 30,
+    loop : true,
+    centeredSlides: true,
+    navigation: {
+        nextEl: '.best_control .swiper-button-next',
+        prevEl: '.best_control .swiper-button-prev',
+    },
+    scrollbar: {
+        el: '#best_seller .swiper-scrollbar',
+        draggable: true,
+        dragSize: 'auto',
+    },
+    autoplay : {delay:3000,},
+    })
+
+/* 포 도그 */
+const forDogSeller = new Swiper('#for_dog .swiper',{
+    slidesPerView : 1,
+    loop : true,
+    centeredSlides: true,
+    navigation: {
+        nextEl: '#for_dog .swiper-button-next',
+        prevEl: '#for_dog .swiper-button-prev',
+    },
+    scrollbar: {
+        el: '#for_dog .swiper-scrollbar',
+        draggable: true,
+        dragSize: 'auto',
+    },
+    autoplay : {delay:3000,},
+    })
+
+/* 포 캣 */
+const forCatSeller = new Swiper('#for_cat .swiper',{
+    slidesPerView : 1,
+    loop : true,
+    centeredSlides: true,
+    navigation: {
+        nextEl: '#for_cat .swiper-button-next',
+        prevEl: '#for_cat .swiper-button-prev',
+    },
+    scrollbar: {
+        el: '#for_cat .swiper-scrollbar',
+        draggable: true,
+        dragSize: 'auto',
+    },
+    autoplay : {delay:3000,},
+    })
